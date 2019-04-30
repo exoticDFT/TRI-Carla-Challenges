8723a0bb6d9881d2a230d67016f18c333b8f4924
Alexander Koufos <exoticdft@gmail.com>
2019-04-30 16:20:19 -0700

    Adds functionality to Event4.py
    - Adds function to parse command line arguments within the program.
    - Adds test functionality to event_4 module

--- Event4.py
+++ Event4.py
@@ -11,15 +11,52 @@ except ImportError:
     )
 
 import argparse
+import random
 
 # Modules
-def event_4(seed):
-    print('Using seed', seed)
+def parse_arguments():
+    argparser = argparse.ArgumentParser(
+        description='TRI Carla Challenge - Event 4',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+    argparser.add_argument(
+        '-s',
+        '--seed',
+        default=None,
+        help='The random number seed used during scenario generation'
+    )
+    argparser.add_argument(
+        '--host',
+        metavar='H',
+        default='127.0.0.1',
+        help='The ip address of the host server'
+    )
+    argparser.add_argument(
+        '--port',
+        metavar='P',
+        default=2000,
+        type=int,
+        help='TCP port used for listening'
+    )
+
+    args = argparser.parse_args()
+    args.description = argparser.description
+
+    return args
+
+
+def event_4(args):
+    # Use provided seed or system time if none is provided
+    random.seed(a=args.seed)
+
+    for x in range(10):
+        print('Random number', x, "-", random.randint(1, 100))
 
 
 # Define the main module
 def main():
-    event_4(4000)
+    args = parse_arguments()
+    event_4(args)
 
 
 # Main execution
