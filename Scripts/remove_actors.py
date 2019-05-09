1487032dd585234a91cd41a39b4c94f63d7ffe36
Alexander Koufos <exoticdft@gmail.com>
2019-05-09 16:07:11 -0700

    Creates script for removing actors from the traffic circle.
    - Creates a script for removing actors from the traffic circle.

---
+++ Scripts/remove_actors.py
@@ -0,0 +1,18 @@
+#!/bin/env/python
+ 
+import Event4
+import util.carla_common as cc
+
+import time
+
+def main():
+    client = cc.create_client()
+
+    world = client.get_world()
+
+    while True:
+        Event4.remove_non_traffic_circle_agents(world, verbose=True)
+        print('Total actors remaining:', len(world.get_actors().filter('vehicle.*')))
+        time.sleep(5.0)
+
+main()
