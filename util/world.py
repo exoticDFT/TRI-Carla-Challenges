088a1298801342158583ebab94a4b8e91f150aee
Alexander Koufos <exoticdft@gmail.com>
2019-05-15 14:57:21 -0700

    issue #14: Adds documentation to the world module.
    - Adds docstring documentation to the draw_spawn_points function in the 
      util library's world module.

--- util/world.py
+++ util/world.py
@@ -1,6 +1,18 @@
 import carla
 
 def draw_spawn_points(world, timeout=-1.0):
+    '''
+    Draws all the available spawn points in the current Carla world.
+
+    Parameters
+    ----------
+    world : carla.World
+        The Carla world in which to draw the spawn point indices.
+    timeout : float, optional
+        The total number of seconds in which to keep the labels in the 
+        environment. (Default is -1.0, which leaves the label in the 
+        environment indefinitely.)
+    '''
     map = world.get_map()
     spawn_points = map.get_spawn_points()
 
